f53565a78d5e26056e47ef2fd7870ef57ea5e0a8
Abhishek Chaudhary <abhishek7gg7@gmail.com>
2020-10-05 09:20:22 +0530

    Added Service Worker

---
+++ sw.js
@@ -0,0 +1,27 @@
+const cacheName = 'files';
+
+addEventListener('fetch',  fetchEvent => {
+  const request = fetchEvent.request;
+  if (request.method !== 'GET') {
+    return;
+  }
+  fetchEvent.respondWith(async function() {
+    const responseFromFetch = fetch(request);
+    fetchEvent.waitUntil(async function() {
+      const responseCopy = (await responseFromFetch).clone();
+      const myCache = await caches.open(cacheName);
+      await myCache.put(request, responseCopy);
+    }());
+    if (request.headers.get('Accept').includes('text/html')) {
+      try {
+        return await responseFromFetch;
+      }
+      catch(error) {
+        return caches.match(request);
+      }
+    } else {
+      const responseFromCache = await caches.match(request);
+      return responseFromCache || responseFromFetch;
+    }
+  }());
+});
